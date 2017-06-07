e4d077251fad1725dbfbb4d1ae12e8dcdf96a6f3
Benjamin Levin <benjaminwlevin@gmail.com>
2017-06-07 17:04:11 -0400

    assignment added

--- checkerboard_assignment/checkerboard_assignment.py
+++ checkerboard_assignment/checkerboard_assignment.py
@@ -1,5 +1,5 @@
 i = 1
-while i < 10: #specifed number determines number of rows in board
+while i < 20: #specifed number determines number of rows in board
     if i % 2 == 1:
         print "* * * * * "
         i = i + 1
