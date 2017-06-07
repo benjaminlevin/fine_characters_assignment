30031c00e0ecccb9234c9a9519307be1e65d5994
Benjamin Levin <benjaminwlevin@gmail.com>
2017-06-07 16:56:34 -0400

    first commit

---
+++ checkerboard_assignment/checkerboard_assignment.py
@@ -0,0 +1,8 @@
+i = 1
+while i < 10: #specifed number determines number of rows in board
+    if i % 2 == 1:
+        print "* * * * * "
+        i = i + 1
+    else:
+        print " * * * * *"
+        i = i + 1
